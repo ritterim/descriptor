0dd156f7fb0a8faf1a84072b2af31cd50d79e1e5
William Boga IV <bill.boga@ritterim.com>
2015-03-18 13:09:34 -0400

    Add HTTP descriptor. This implementation inherits from the base descriptor class and adds properties for URI and relationship.

---
+++ src/Descriptor/Generic/HttpDescriptorContainer`1.cs
@@ -0,0 +1,22 @@
+﻿namespace RimDev.Descriptor.Generic
+{
+    public class HttpDescriptorContainer<T> : DescriptorContainer<T>
+    {
+        public virtual string Rel { get; set; }
+        public virtual string Uri { get; set; }
+
+        public HttpDescriptorContainer<T> SetRel(string rel)
+        {
+            Rel = rel;
+
+            return this;
+        }
+
+        public HttpDescriptorContainer<T> SetUri(string uri)
+        {
+            Uri = uri;
+
+            return this;
+        }
+    }
+}
